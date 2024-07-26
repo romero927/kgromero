ec6e3d70d4963fd37e05d30a7c7d9ada81616148
Kyle Romero <kgromero@gmail.com>
2024-07-26 10:42:21 -0400

    Add Detailed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -180,6 +180,10 @@
         <DropdownItem href={ResumeTXT} target="_blank"
           >Resume - TXT</DropdownItem
         >
+        <DropdownDivider />
+        <DropdownItem href={ResumeDetailedPDF} target="_blank"
+          >Resume - Detailed PDF</DropdownItem
+        >
       </Dropdown>
       <NavLi class="my-2 cursor-pointer text-black/60 dark:text-gray-400">
         Supporting Documents<ChevronDownOutline
