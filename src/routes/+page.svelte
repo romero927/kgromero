d151038f3e735935b1391bc762f05cd21190c0a2
Kyle Romero <kgromero@gmail.com>
2024-07-29 11:47:42 -0400

    Add divider

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -160,11 +160,17 @@
       <img class="w-11 mr-2" alt="logo" src={logo} />
     </NavBrand>
     <NavHamburger />
-    <NavUl ulClass="border-none flex flex-col p-4 mt-4 bg-gray-50 dark:bg-gray-800 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:text-sm md:font-medium">
-      <DropdownDivider divClass="my-1 h-px bg-gray-100 dark:bg-gray-600 lg:display-none"/>
-      <NavLi href={BusinessCard} class="my-2 cursor-pointer text-black/60 dark:text-gray-400" target="_blank"
-          >Business Card</NavLi
-        >
+    <NavUl
+      ulClass="border-none flex flex-col p-4 mt-4 bg-gray-50 dark:bg-gray-800 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:text-sm md:font-medium"
+    >
+      <DropdownDivider
+        divClass="my-1 h-px bg-gray-100 dark:bg-gray-600 lg:display-none"
+      />
+      <NavLi
+        href={BusinessCard}
+        class="my-2 cursor-pointer text-black/60 dark:text-gray-400"
+        target="_blank">Business Card</NavLi
+      >
       <NavLi class="my-2 cursor-pointer text-black/60 dark:text-gray-400">
         Resume<ChevronDownOutline
           class="w-6 h-6 ms-2 text-primary-800 dark:text-white inline"
@@ -193,7 +199,7 @@
         <DropdownDivider />
         <DropdownItem href={CoverLetter} target="_blank"
           >Cover Letter</DropdownItem
-        >        
+        >
       </Dropdown>
       <NavLi class="my-2 cursor-pointer text-black/60 dark:text-gray-400">
         Supporting Documents<ChevronDownOutline
@@ -375,14 +381,29 @@
           >
         </Dropdown>
       </Dropdown>
-      <DropdownDivider divClass="my-1 h-px bg-gray-100 dark:bg-gray-600 lg:display-none"/>
-      <NavLi href="https://github.com/romero927/kgromero" target="_blank" class="sm:my-2 md:my-0 cursor-pointer">
-        <div class="rounded-lg text-gray-500 dark:text-gray-400 hover:bg-gray-50 hover:text-red-500 dark:hover:bg-gray-700 dark:hover:text-white sm:ml-0 sm:pl-0 sm:pt-1 sm:pb-1 sm:mb-1 md:p-3">
-          <Fa class="p-0 m-0" icon={faCode}/>
+      <DropdownDivider
+        divClass="my-1 h-px bg-gray-100 dark:bg-gray-600 lg:display-none"
+      />
+      <NavLi
+        href="https://github.com/romero927/kgromero"
+        target="_blank"
+        class="sm:my-2 md:my-0 cursor-pointer"
+      >
+        <div
+          class="rounded-lg text-gray-500 dark:text-gray-400 hover:bg-gray-50 hover:text-red-500 dark:hover:bg-gray-700 dark:hover:text-white sm:ml-0 sm:pl-0 sm:pt-1 sm:pb-1 sm:mb-1 md:p-3"
+        >
+          <Fa class="p-0 m-0" icon={faCode} />
         </div>
       </NavLi>
-      <DropdownDivider divClass="my-1 h-px bg-gray-100 dark:bg-gray-600 lg:display-none"/>
-        <DarkMode btnClass="text-gray-500 sm:my-2 md:my-0 dark:text-gray-400 hover:text-red-500 hover:bg-gray-50 dark:hover:bg-gray-700 dark:hover:text-white focus:outline-none rounded-lg sm:rounded-md text-sm p-2.5 "/>
+      <DropdownDivider
+        divClass="my-1 h-px bg-gray-100 dark:bg-gray-600 lg:display-none"
+      />
+      <DarkMode
+        btnClass="text-gray-500 sm:my-2 md:my-0 dark:text-gray-400 hover:text-red-500 hover:bg-gray-50 dark:hover:bg-gray-700 dark:hover:text-white focus:outline-none rounded-lg sm:rounded-md text-sm p-2.5 "
+      />
+      <DropdownDivider
+        divClass="my-1 h-px bg-gray-100 dark:bg-gray-600 lg:display-none"
+      />
     </NavUl>
   </Navbar>
   <div
@@ -399,7 +420,8 @@
       <b class="pt-1 pb-3">About Me</b>
       <Hr classHr="border w-full m-1" />
       <br />
-      <img class="w-1/2 rounded-full m-1" alt="Kyle Romero" src={Profile} /><br />
+      <img class="w-1/2 rounded-full m-1" alt="Kyle Romero" src={Profile} /><br
+      />
       <Hr classHr="border w-full m-1" />
       <br />
       IT Leader with 10 years as a full-stack developer and 7 years in software development
