600d3aaf2b3c6ef3ed5d0e54c41b294c308fe935
Kyle Romero <kgromero@gmail.com>
2024-09-03 21:07:17 -0400

    Update SEO data

--- src/lib/seoData.js
+++ src/lib/seoData.js
@@ -2,14 +2,14 @@
 export const seoData = {
     defaultTitle: "Kyle Romero",
     defaultDescription: "Kyle Romero is a Software Engineering Leader with extensive experience in IT, based in Jersey City, NJ. Specializing in full-stack development and team management.",
-    defaultImage: "https://www.kgromero.com/images/kgromero.png", // Replace with your actual image URL
-    siteUrl: "https://www.kgromero.com", // Replace with your actual site URL
+    defaultImage: "https://kgromero.com/images/kgromero.png", // Replace with your actual image URL
+    siteUrl: "https://kgromero.com", // Replace with your actual site URL
     defaultStructuredData: {
       "@context": "https://schema.org",
       "@type": "Person",
       "name": "Kyle Romero",
       "jobTitle": "Software Engineering Leader",
-      "url": "https://www.kgromero.com",
+      "url": "https://kgromero.com",
       "sameAs": [
         "https://www.linkedin.com/in/kyleromero/",
         "https://github.com/romero927"
