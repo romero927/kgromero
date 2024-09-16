71a797c75eca4eb2ebaeb548d5cf3270e19cbb6a
Kyle Romero <kgromero@gmail.com>
2024-09-16 13:22:15 -0400

    update divider location

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -194,8 +194,8 @@
         <DropdownItem href="https://kgromero-react.netlify.app/" target="_blank">Old Site (React)</DropdownItem>
       </Dropdown>
     </Dropdown>
-    <DropdownDivider divClass="my-1 h-px bg-gray-100 dark:bg-gray-600 lg:display-none" />
     <NavLi href={BusinessCard} class="my-2 cursor-pointer text-black/60 dark:text-gray-400" target="_blank">Business Card</NavLi>
+    <DropdownDivider divClass="my-1 h-px bg-gray-100 dark:bg-gray-600 lg:display-none" />
     <NavLi href="https://github.com/romero927/kgromero" target="_blank" class="sm:my-2 md:my-0 cursor-pointer">
       <div class="rounded-lg text-gray-500 dark:text-gray-400 hover:bg-gray-50 hover:text-red-500 dark:hover:bg-gray-700 dark:hover:text-white sm:ml-0 sm:pl-0 sm:pt-1 sm:pb-1 sm:mb-1 md:p-3">
         <Fa class="p-0 m-0" icon={faCode} />
