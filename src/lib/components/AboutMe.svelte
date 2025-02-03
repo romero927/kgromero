9a9729f57cb2eaf60581c5113d8de584c283a866
Kyle Romero <kgromero@gmail.com>
2025-02-03 13:01:57 -0500

    Updates

--- src/lib/components/AboutMe.svelte
+++ src/lib/components/AboutMe.svelte
@@ -8,14 +8,16 @@
 <div class="neo-bold-card items-center flex flex-col max-w-full h-full bg-white " padding="xs">
   <b class="pt-1 pb-3">{$t.aboutMe.title}</b>
   <Hr classHr="border w-full m-2" />
-    <li class="flex space-x-2 ">
-      <img 
-          class="h-20 w-20 rounded-full m-1 lazy-image" 
+    <div class="lg:flex lg:space-x-2">
+      <div class="">
+        <img 
+          class="w-full max-w-[200px] mx-auto rounded-full m-1 lazy-image" 
           alt="Kyle Romero" 
           src="/images/kgromero.webp"
         />
-        <div class="text-base">{$t.aboutMe.content}</div>
-    </li>
+      </div>
+      <div class="text-base">{$t.aboutMe.content}</div>
+    </div>
     <br />
     <Hr classHr="border w-full m-4" />
     <br />
